79faecb9c57fa981cea365ec397df22da46867e5
eryshandy22 <73053784+eryshandy22@users.noreply.github.com>
2023-07-03 22:08:37 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tesla News',
+      title: 'Apple News',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  List<dynamic> favoriteArticles = [];
+
+  void addToFavorites(dynamic article) {
+    favoriteArticles.add(article);
+    notifyListeners();
+  }
+
+  void removeFromFavorites(dynamic article) {
+    favoriteArticles.remove(article);
+    notifyListeners();
   }
 }
 
@@ -34,10 +54,10 @@ class _HomePageState extends State<HomePage> {
     if (isLoading || isLastPage) return;
 
     final apiKey = '2e8488957a83447eaa39850372f3d93b';
-    final pageSize = 10;
+    final pageSize = 4;
 
     final url =
-        'https://newsapi.org/v2/everything?q=tesla&from=2023-06-03&sortBy=publishedAt&apiKey=$apiKey&page=$page&pageSize=$pageSize';
+        'https://newsapi.org/v2/everything?q=apple&from=2023-07-02&to=2023-07-02&sortBy=popularity&apiKey=$apiKey&page=$page&pageSize=$pageSize';
 
     setState(() {
       isLoading = true;
@@ -94,7 +114,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tesla News'),
+        title: Text('Apple News'),
       ),
       body: ListView.builder(
         itemCount: articles.length + 1,
@@ -116,6 +136,10 @@ class _HomePageState extends State<HomePage> {
           }
 
           final article = articles[index];
+          final isFavorite = Provider.of<MyAppState>(context)
+              .favoriteArticles
+              .contains(article);
+
           return Card(
             margin: EdgeInsets.all(8.0),
             child: ListTile(
@@ -129,6 +153,22 @@ class _HomePageState extends State<HomePage> {
                   : Container(),
               title: Text(article['title']),
               subtitle: Text(article['description']),
+              trailing: IconButton(
+                icon: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: isFavorite ? Colors.red : null,
+                ),
+                onPressed: () {
+                  final appState =
+                      Provider.of<MyAppState>(context, listen: false);
+
+                  if (isFavorite) {
+                    appState.removeFromFavorites(article);
+                  } else {
+                    appState.addToFavorites(article);
+                  }
+                },
+              ),
               onTap: () {
                 // Add the desired action when the item is tapped
                 print('Article tapped: ${article['title']}');
@@ -163,7 +203,44 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<dynamic> searchResults = [];
+  bool isLoading = false;
+
+  Future<void> searchNews(String keyword) async {
+    if (isLoading) return;
+
+    setState(() {
+      isLoading = true;
+      searchResults.clear();
+    });
+
+    final apiKey = '2e8488957a83447eaa39850372f3d93b';
+    final pageSize = 4;
+
+    final url =
+        'https://newsapi.org/v2/everything?q=$keyword&from=2023-06-03&sortBy=publishedAt&apiKey=$apiKey&page=1&pageSize=$pageSize';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final newArticles = jsonData['articles'] as List<dynamic>;
+
+      setState(() {
+        searchResults.addAll(newArticles);
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to search news');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -180,16 +257,60 @@ class SearchPage extends StatelessWidget {
                 prefixIcon: Icon(Icons.search),
               ),
               onChanged: (value) {
-                // Perform search operation based on the entered value
+                searchNews(value);
               },
             ),
           ),
           Expanded(
-            child: ListView(
-              children: [
-                // Display search results here
-              ],
-            ),
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    itemCount: searchResults.length,
+                    itemBuilder: (context, index) {
+                      final article = searchResults[index];
+                      final isFavorite = Provider.of<MyAppState>(context)
+                          .favoriteArticles
+                          .contains(article);
+
+                      return Card(
+                        margin: EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: article['urlToImage'] != null
+                              ? Image.network(
+                                  article['urlToImage'],
+                                  width: 100,
+                                  height: 100,
+                                  fit: BoxFit.cover,
+                                )
+                              : Container(),
+                          title: Text(article['title']),
+                          subtitle: Text(article['description']),
+                          trailing: IconButton(
+                            icon: Icon(
+                              isFavorite
+                                  ? Icons.favorite
+                                  : Icons.favorite_border,
+                              color: isFavorite ? Colors.red : null,
+                            ),
+                            onPressed: () {
+                              final appState = Provider.of<MyAppState>(context,
+                                  listen: false);
+
+                              if (isFavorite) {
+                                appState.removeFromFavorites(article);
+                              } else {
+                                appState.addToFavorites(article);
+                              }
+                            },
+                          ),
+                          onTap: () {
+                            // Add the desired action when the item is tapped
+                            print('Article tapped: ${article['title']}');
+                          },
+                        ),
+                      );
+                    },
+                  ),
           ),
         ],
       ),
@@ -204,8 +325,49 @@ class FavoritePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Favorites'),
       ),
-      body: Center(
-        child: Text('Favorite articles'),
+      body: Consumer<MyAppState>(
+        builder: (context, appState, _) {
+          final favoriteArticles = appState.favoriteArticles;
+
+          if (favoriteArticles.isEmpty) {
+            return Center(child: Text('No favorite articles'));
+          }
+
+          return ListView.builder(
+            itemCount: favoriteArticles.length,
+            itemBuilder: (context, index) {
+              final article = favoriteArticles[index];
+
+              return Card(
+                margin: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: article['urlToImage'] != null
+                      ? Image.network(
+                          article['urlToImage'],
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        )
+                      : Container(),
+                  title: Text(article['title']),
+                  subtitle: Text(article['description']),
+                  trailing: IconButton(
+                    icon: Icon(Icons.favorite, color: Colors.red),
+                    onPressed: () {
+                      final appState =
+                          Provider.of<MyAppState>(context, listen: false);
+                      appState.removeFromFavorites(article);
+                    },
+                  ),
+                  onTap: () {
+                    // Add the desired action when the item is tapped
+                    print('Favorite article tapped: ${article['title']}');
+                  },
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
